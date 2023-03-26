8a8c9ebb70be1c4e72b693f75cf06c9c0ca2ecfc
vuvanphuongofficial <phuongxoan2k1@gmail.com>
2023-03-26 13:15:34 +0700

    init project

---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+  <div class="col-md-4">
+
+          <!-- Search Widget -->
+          <div class="card mb-4">
+            <h5 class="card-header">Tìm kiếm</h5>
+            <div class="card-body">
+                   <form name="search" action="search.php" method="post">
+              <div class="input-group">
+           
+        <input type="text" name="searchtitle" class="form-control" placeholder="Thông tin tìm kiếm..." required>
+                <span class="input-group-btn">
+                  <button class="btn btn-secondary" type="submit">Go!</button>
+                </span>
+              </form>
+              </div>
+            </div>
+          </div>
+
+          <!-- Categories Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Thể loại tin tức</h5>
+            <div class="card-body">
+              <div class="row">
+                <div class="col-lg-6">
+                <h6> Đang hoàn thiện </h6>
+                </div>
+       
+              </div>
+            </div>
+          </div>
+
+          <!-- Side Widget -->
+          <div class="card my-4">
+            <h5 class="card-header">Tin tức gần đây</h5>
+            <div class="card-body">
+              <h6> Đang hoàn thiện </h6>
+            </div>
+          </div>
+
+        </div>
---
+++ news-details.php
@@ -0,0 +1,126 @@
+<?php 
+session_start();
+include('includes/config.php');
+//Genrating CSRF Token
+if (empty($_SESSION['token'])) {
+ $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+
+if(isset($_POST['submit']))
+{
+  //Verifying CSRF Token
+if (!empty($_POST['csrftoken'])) {
+    if (hash_equals($_SESSION['token'], $_POST['csrftoken'])) {
+$name=$_POST['name'];
+$email=$_POST['email'];
+$comment=$_POST['comment'];
+$postid=intval($_GET['nid']);
+$st1='0';
+$query=mysqli_query($con,"insert into tblcomments(postId,name,email,comment,status) values('$postid','$name','$email','$comment','$st1')");
+if($query):
+  echo "<script>alert('comment successfully submit. Comment will be display after admin review ');</script>";
+  unset($_SESSION['token']);
+else :
+ echo "<script>alert('Something went wrong. Please try again.');</script>";  
+
+endif;
+
+}
+}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Trang tin tức | Mô tả</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/modern-business.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+    <!-- Navigation -->
+   <?php include('includes/header.php');?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+
+     
+      <div class="row" style="margin-top: 4%">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <!-- Blog Post -->
+<?php
+$pid=intval($_GET['nid']);
+ $query=mysqli_query($con,"select tblposts.PostTitle as posttitle,tblposts.PostImage,tblcategory.CategoryName as category,tblcategory.id as cid,tblsubcategory.Subcategory as subcategory,tblposts.PostDetails as postdetails,tblposts.PostingDate as postingdate,tblposts.PostUrl as url from tblposts left join tblcategory on tblcategory.id=tblposts.CategoryId left join  tblsubcategory on  tblsubcategory.SubCategoryId=tblposts.SubCategoryId where tblposts.id='$pid'");
+while ($row=mysqli_fetch_array($query)) {
+?>
+
+          <div class="card mb-4">
+      
+            <div class="card-body">
+              <h2 class="card-title"><?php echo htmlentities($row['posttitle']);?></h2>
+              <p><b>Thể loại : </b> <a href="category.php?catid=<?php echo htmlentities($row['cid'])?>"><?php echo htmlentities($row['category']);?></a> |
+                <b>Tiểu mục : </b><?php echo htmlentities($row['subcategory']);?> <b> Đăng vào lúc </b><?php echo htmlentities($row['postingdate']);?></p>
+                <hr />
+
+ <img class="img-fluid rounded" src="admin/postimages/<?php echo htmlentities($row['PostImage']);?>" alt="<?php echo htmlentities($row['posttitle']);?>">
+  
+              <p class="card-text"><?php 
+$pt=$row['postdetails'];
+              echo  (substr($pt,0));?></p>
+             
+            </div>
+            <div class="card-footer text-muted">
+             
+           
+            </div>
+          </div>
+<?php } ?>
+       
+
+      
+
+     
+
+        </div>
+
+        <!-- Sidebar Widgets Column -->
+      <?php include('includes/sidebar.php');?>
+      </div>
+      <!-- /.row -->
+<!---Comment Section --- 
+
+<?php  ?>
+
+        </div>
+      </div>
+    </div>
+
+  
+      <?php include('includes/footer.php');?>
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
